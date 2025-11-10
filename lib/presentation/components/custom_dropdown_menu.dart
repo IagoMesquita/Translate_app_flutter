e2b9ecc90d73670f4978a447d2cf1cf9d64dde4b
Iago Mesquita <iagomesquitaalves@gmail.com>
2025-11-10 09:09:25 -0300

    feat: adiciona parametrod necessarios

--- lib/presentation/components/custom_dropdown_menu.dart
+++ lib/presentation/components/custom_dropdown_menu.dart
@@ -1,18 +1,35 @@
 import 'package:flutter/material.dart';
 
 class CustomDropdownMenu extends StatelessWidget {
-  const CustomDropdownMenu({super.key});
+  const CustomDropdownMenu({
+    super.key,
+    this.onSelected,
+    required this.languages,
+    this.initialSelection,
+  });
+
+  final void Function(String?)? onSelected;
+  final List<String> languages;
+  final String? initialSelection;
 
   @override
   Widget build(BuildContext context) {
-    return DropdownMenu(
-      onSelected: (value) {},
-      menuStyle: MenuStyle(alignment: Alignment.center),
-      textAlign: TextAlign.center,
-      initialSelection: "Language",
-      trailingIcon: Icon(Icons.arrow_drop_down, color: Colors.white),
-      selectedTrailingIcon: Icon(Icons.arrow_drop_up, color: Colors.white),
-      dropdownMenuEntries: [],
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: DropdownMenu(
+        onSelected: onSelected,
+        menuStyle: MenuStyle(alignment: Alignment.center),
+        textAlign: TextAlign.center,
+        textStyle: TextStyle(color: Colors.white),
+        initialSelection: initialSelection,
+        trailingIcon: Icon(Icons.arrow_drop_down, color: Colors.white),
+        selectedTrailingIcon: Icon(Icons.arrow_drop_up, color: Colors.white),
+        dropdownMenuEntries: languages
+            .map(
+              (language) => DropdownMenuEntry(value: language, label: language),
+            )
+            .toList(),
+      ),
     );
   }
 }
